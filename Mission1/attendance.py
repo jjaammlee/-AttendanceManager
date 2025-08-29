b763bb661ee26cd26351ec124b9b26655198669b
jjaamm.lee <dlwoa93@naver.com>
2025-08-29 11:40:17 +0900

    [clean code_ver2]

--- Mission1/attendance.py
+++ Mission1/attendance.py
@@ -1,24 +1,19 @@
 from platform import mac_ver
 
-id_lst = {}
-max_id = 0
-points = [0] * 100
-grade = {}
-names = [''] * 100
 wed = [0] * 100
 weekend = [0] * 100
 
 
-def get_id(name):
-    global max_id
-    if name not in id_lst:
+def get_id(name, id_dict, names):
+    max_id = len(id_dict)
+    if name not in id_dict:
         max_id += 1
-        id_lst[name] = max_id
+        id_dict[name] = max_id
         names[max_id] = name
+    return id_dict, names
 
 
-def get_attendance_points(name, day):
-    id = id_lst[name]
+def get_attendance_points(id, day, points):
     add_point = 0
     if day in ["monday", "tuesday", "thursday", "friday"]:
         add_point += 1
@@ -29,17 +24,20 @@ def get_attendance_points(name, day):
         add_point += 2
         weekend[id] += 1
     points[id] += add_point
+    return points
 
 
-def get_bonus_points():
+def get_bonus_points(points, max_id):
     for id in range(1, max_id + 1):
         if wed[id] > 9:
             points[id] += 10
         if weekend[id] > 9:
             points[id] += 10
+    return points
 
 
-def get_grade():
+def get_grade(points, max_id, names):
+    grade = {}
     for id in range(1, max_id + 1):
         if points[id] >= 50:
             grade[id] = (1, "GOLD")
@@ -48,9 +46,10 @@ def get_grade():
         else:
             grade[id] = (0, "NORMAL")
         print(f"NAME : {names[id]}, POINT : {points[id]}, GRADE : {grade[id][1]}")
+    return grade
 
 
-def show_removed_player():
+def show_removed_player(grade, max_id, names):
     print("\nRemoved player")
     print("==============")
     for id in range(1, max_id + 1):
@@ -60,6 +59,9 @@ def show_removed_player():
 
 def input_file():
     try:
+        points = [0] * 100
+        names = [''] * 100  # id -> name
+        id_dict = {}        # name -> id
         with open("attendance_weekday_500.txt", encoding='utf-8') as f:
             for _ in range(500):
                 line = f.readline()
@@ -67,11 +69,11 @@ def input_file():
                     break
                 parts = line.strip().split()
                 if len(parts) == 2:
-                    get_id(parts[0])
-                    get_attendance_points(parts[0], parts[1])
-        get_bonus_points()
-        get_grade()
-        show_removed_player()
+                    id_dict, names =  get_id(parts[0], id_dict, names)
+                    points = get_attendance_points(id_dict[parts[0]], parts[1], points)
+        points = get_bonus_points(points, len(id_dict))
+        grade = get_grade(points, len(id_dict), names)
+        show_removed_player(grade, len(id_dict), names)
 
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
